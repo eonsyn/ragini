630ce35ba2680b11a605ffe8c3cf5961749bc4e1
eonsyn <154956467+eonsyn@users.noreply.github.com>
2025-02-08 19:06:02 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        loveRed: "#E63946",
+        blushPink: "#F4A7B9",
+        goldenGlow: "#FFD166",
+        elegantIvory: "#FFF5E1",
+        deepWine: "#6D214F",
+      },
+    },
+  },
+  plugins: [],
+};
